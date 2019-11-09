fd15a38eba4018ef5396679bb020dda77d303046
Rodney "XMeintek" Lee <49397250+JustRodneyLee@users.noreply.github.com>
2019-11-09 13:28:00 +0800

    Added BeefyScript.cs

---
+++ BeefyGameStudio/Engine/BeefyScript.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeefyGameStudio.Engine
+{
+    public class BeefyScript
+    {
+        BeefyScript()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Invoke()
+        {
+
+        }
+
+    }
+}
